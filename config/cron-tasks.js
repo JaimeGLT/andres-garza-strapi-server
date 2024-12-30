6d799188075ad0a7b3ea66a6430e5661a11e7837
JaimeGLT <jaimeguzmanlt@gmail.com>
2024-12-30 08:09:21 -0400

    ulitmo cron

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const { VITE_URL_VIDEOS_SHORTS, VITE_KEY, VITE_URL_VIDEOS_POPULARES, VITE_URL_VIDEOS_RECIENTES } = process.env;
 
 module.exports = {
-  '44 7 * * *': async ({ strapi }) => {
+  '15 8 * * *': async ({ strapi }) => {
     try {
       const response = await axios.get(`${VITE_URL_VIDEOS_POPULARES}${VITE_KEY}`);
       
