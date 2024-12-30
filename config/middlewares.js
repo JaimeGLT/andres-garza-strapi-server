96af66d9ef6eacee7f65d7dab97f2c502f6c6ffa
JaimeGLT <jaimeguzmanlt@gmail.com>
2024-12-30 10:09:22 -0400

    añadiendo middleware security

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,6 +7,7 @@ module.exports = [
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     },
   },
+  'strapi::security',
   'strapi::logger',
   'strapi::cors',
   'strapi::poweredBy',
