76396845bf3431789942477b6450e0449127fb69
JaimeGLT <jaimeguzmanlt@gmail.com>
2024-12-29 20:50:01 -0400

    profavor-backend

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,12 @@
 module.exports = [
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://client-d4ywyeuxi-jaimeguzmanlt-gmailcoms-projects.vercel.app/'], // Agrega el dominio de tu frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    },
+  },
+
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
