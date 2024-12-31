c72aa20c71069d96c798f1708ecc612bbe3e8273
JaimeGLT <jaimeguzmanlt@gmail.com>
2024-12-31 15:56:28 -0400

    cambiado apikey nombre

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,13 @@
 module.exports = [
   'strapi::errors',
-  {
+// En Strapi, en el archivo config/middleware.js o config/env/production/middleware.js
+{
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'https://client-opal-xi-12.vercel.app'], // Agrega el dominio de tu frontend
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+        origin: ['http://localhost:5173', 'https://client-opal-xi-12.vercel.app'], // Asegúrate de que este esté correctamente configurado
     },
-  },
+},
+
   {
     name: 'strapi::security',
     config: {
